e908953f73685e89b973efc8b280e32eb913d781
Shubham Thakre <thakre.k.s@gmail.com>
2023-06-13 15:32:29 +0530

    fifth commit

--- JAVA/BASIC/B_HiWorld.java
+++ JAVA/BASIC/B_HiWorld.java
@@ -4,6 +4,7 @@ public class B_HiWorld {
     
     public static void main(String[] args) {
         System.out.println("hi shubham you are just awsome");
+        System.out.println("Singh is king");
     }
     
 }
