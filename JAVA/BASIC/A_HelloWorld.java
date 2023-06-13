eaced40fe544a7eec5d470bd01595eed061e2398
Shubham Kihorrao Thakre <thakre.k.s@gmail.com>
2023-06-13 12:25:31 +0530

    "Secod Commit"

--- JAVA/BASIC/A_HelloWorld.java
+++ JAVA/BASIC/A_HelloWorld.java
@@ -2,6 +2,8 @@ class A_HelloWorld
 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        System.out.println("I am Shubham thakre");
 
     }
 }
